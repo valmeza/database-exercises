123fa50f5e830d94d10b453cc6b78154217e30bb
Valeria Meza <valeriameza173@gmail.com>
2020-05-26 15:58:03 -0500

    cleaned up code.

--- select_exercises.sql
+++ select_exercises.sql
@@ -1,21 +1,12 @@
 USE codeup_test_db;
 
-SELECT name
-FROM albums
-WHERE artist = 'Pink Floyd';
+SELECT 'Pink Floyd Albums' AS 'Exercise 1';
+SELECT * FROM albums WHERE artist = 'Pink Floyd'
 
-SELECT release_date
-FROM albums
-WHERE name = 'Sgt. Pepper''s Lonely Hearts Club Band';
+SELECT release_date FROM albums WHERE name = 'Sgt. Pepper''s Lonely Hearts Club Band';
 
-SELECT genre
-FROM albums
-WHERE name = 'Nevermind';
+SELECT genre FROM albums WHERE name = 'Nevermind';
 
-SELECT name
-FROM albums
-WHERE sales <= 20.0;
+SELECT name FROM albums WHERE sales <= 20.0;
 
-SELECT name
-FROM albums
-WHERE genre = 'Rock';
+SELECT name FROM albums WHERE genre = 'Rock';
--- update_exercises.sql
+++ update_exercises.sql
@@ -1,11 +1,17 @@
 USE codeup_test_db;
 
 SELECT name FROM albums;
-UPDATE albums SET sales = sales * 10;
+-- UPDATE albums SET sales = sales * 10;
+
+SELECT sales FROM albums;
 
 SELECT name FROM albums WHERE release_date <= 1980;
 UPDATE albums SET release_date = 1800 WHERE release_date < 1980;
 
+SELECT release_date FROM albums;
+
 SELECT name FROM albums WHERE artist = 'Michael Jackson';
 UPDATE albums SET artist = 'Peter Jackson' WHERE artist = 'Michael Jackson';
+
+SELECT artist FROM albums;
 
