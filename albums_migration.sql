a770ffb3036e5cf2d0d28464e1f98efcf65c6303
Valeria Meza <valeriameza173@gmail.com>
2020-05-26 10:27:48 -0500

    completed exercise.

--- albums_migration.sql
+++ albums_migration.sql
@@ -1,9 +1,13 @@
+USE codeup_test_db;
+
+DROP TABLE IF EXISTS albums;
+
 CREATE TABLE IF NOT EXISTS albums(
     id INT UNSIGNED NOT NULL AUTO_INCREMENT,
     artist varchar(50) DEFAULT NULL,
     name varchar(50) DEFAULT NULL,
-    release_date YEAR,
-    sales FLOAT,
+    release_date YEAR NOT NULL,
+    sales DECIMAL(6, 3) NOT NULL,
     genre varchar(100) DEFAULT NULL,
     PRIMARY KEY (id)
 );
