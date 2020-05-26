a4c523fb1b275fa4e407340a7e41628cd8ae71c4
Valeria Meza <valeriameza173@gmail.com>
2020-05-26 16:17:08 -0500

    Testing the select part.

--- select_exercises.sql
+++ select_exercises.sql
@@ -1,12 +1,20 @@
 USE codeup_test_db;
 
 SELECT 'Pink Floyd Albums' AS 'Exercise 1';
-SELECT * FROM albums WHERE artist = 'Pink Floyd'
+SELECT * FROM albums WHERE artist = 'Pink Floyd';
 
+
+SELECT 'Sgt. Pepper''s Lonely Hearts Club Band' AS 'Exercise 1';
 SELECT release_date FROM albums WHERE name = 'Sgt. Pepper''s Lonely Hearts Club Band';
 
+SELECT 'Nevermind Genre' AS 'Exercise 1';
 SELECT genre FROM albums WHERE name = 'Nevermind';
 
-SELECT name FROM albums WHERE sales <= 20.0;
+SELECT 'Released 1990-1999' AS 'Exercise 1';
+SELECT * FROM albums WHERE release_date BETWEEN 1990 AND 1999;
 
-SELECT name FROM albums WHERE genre = 'Rock';
+SELECT 'Sales < 20' AS 'Exercise 1';
+SELECT * FROM albums WHERE sales < 20;
+
+SELECT 'Rock Genre' AS 'Exercise 1';
+SELECT * FROM albums WHERE genre = 'Rock';
