27edbf9bb3f2aec30cef3536baacc2249b9d3e82
Valeria Meza <valeriameza173@gmail.com>
2020-05-27 10:18:10 -0500

    minor changes.

--- update_exercises.sql
+++ update_exercises.sql
@@ -1,20 +1,15 @@
 USE codeup_test_db;
 
-SELECT name FROM albums;
+SELECT 'sales' AS 'Exercise 2';
+# SELECT name FROM albums;
 UPDATE albums SET sales = sales * 10;
 
-SELECT 'sales' AS 'Exercise 2';
-SELECT sales FROM albums;
-
-SELECT name FROM albums WHERE release_date <= 1980;
+SELECT 'Date 1800' AS 'Exercise 2';
+# SELECT name FROM albums WHERE release_date <= 1980;
 UPDATE albums SET release_date = 1800 WHERE release_date < 1980;
 
-SELECT 'Date 1800' AS 'Exercise 2';
-SELECT release_date FROM albums;
-
-SELECT name FROM albums WHERE artist = 'Michael Jackson';
+SELECT 'Peter Jackson' AS 'Exercise 2';
+# SELECT name FROM albums WHERE artist = 'Michael Jackson';
 UPDATE albums SET artist = 'Peter Jackson' WHERE artist = 'Michael Jackson';
 
-SELECT 'Peter Jackson' AS 'Exercise 2';
-SELECT artist FROM albums;
 
