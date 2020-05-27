96dc6e052e2f92d0d9655175b12a9d4f1a0a9ced
Valeria Meza <valeriameza173@gmail.com>
2020-05-26 19:08:33 -0500

    completed the delete exercise.

--- delete_exercises.sql
+++ delete_exercises.sql
@@ -1,10 +1,13 @@
 USE codeup_test_db;
 
 SELECT 'Albums Released after 1991' AS 'Exercise 3';
-SELECT name FROM albums WHERE release_date > 1991;
+-- SELECT name FROM albums WHERE release_date > 1991;
+DELETE FROM albums WHERE release_date > 1991;
 
 SELECT 'Albums Genre Disco' AS 'Exercise 3';
-SELECT name FROM albums WHERE genre = 'disco';
+-- SELECT name FROM albums WHERE genre = 'disco';
+DELETE FROM albums WHERE genre = 'disco';
 
 SELECT 'Albums by Nirvana' AS 'Exercise 3';
-SELECT name FROM albums WHERE artist = 'Nirvana';
+-- SELECT name FROM albums WHERE artist = 'Nirvana';
+DELETE FROM albums WHERE  artist = 'Nirvana';
