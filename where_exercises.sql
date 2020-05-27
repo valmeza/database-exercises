846332e11d91cd15622db45eaf46789da7e7cd07
Valeria Meza <valeriameza173@gmail.com>
2020-05-27 15:54:32 -0500

    minor change.

--- where_exercises.sql
+++ where_exercises.sql
@@ -2,7 +2,8 @@ USE employees;
 
 SELECT *
 FROM employees
-WHERE gender = 'M' AND( first_name = 'Irena' OR first_name= 'Vidya' OR first_name = 'Maya');
+WHERE gender = 'M'
+  AND (first_name = 'Irena' OR first_name = 'Vidya' OR first_name = 'Maya');
 
 SELECT *
 FROM employees
@@ -22,11 +23,13 @@ WHERE last_name LIKE '%q%';
 
 SELECT *
 FROM employees
-WHERE last_name LIKE 'E%' OR last_name LIKE '%E';
+WHERE last_name LIKE 'E%'
+   OR last_name LIKE '%E';
 
 SELECT *
 FROM employees
-WHERE last_name LIKE 'E%' AND last_name LIKE '%E';
+WHERE last_name LIKE 'E%'
+  AND last_name LIKE '%E';
 
 SELECT *
 FROM employees
@@ -35,4 +38,5 @@ WHERE hire_date BETWEEN '1990-01-01' AND '1999-12-31'
 
 SELECT *
 FROM employees
-WHERE last_name LIKE '%q%' AND last_name NOT LIKE '%qu%';
+WHERE last_name LIKE '%q%'
+  AND last_name NOT LIKE '%qu%';
