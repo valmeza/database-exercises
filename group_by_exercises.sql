5ef0d96b2d3cf4b4ca37edf65043e50cba25e59e
Valeria Meza <valeriameza173@gmail.com>
2020-05-28 10:47:53 -0500

    completed group exercise.

---
+++ group_by_exercises.sql
@@ -0,0 +1,29 @@
+USE employees;
+
+SELECT DISTINCT title
+FROM titles;
+
+SELECT DISTINCT last_name,
+                COUNT(last_name)
+FROM employees
+WHERE last_name LIKE 'E%'
+  AND last_name LIKE '%E'
+GROUP BY last_name;
+
+SELECT DISTINCT first_name, COUNT(first_name),
+                last_name, COUNT(last_name)
+FROM employees
+WHERE (last_name LIKE 'E%')
+AND (last_name LIKE '%E')
+GROUP BY first_name, last_name;
+
+SELECT DISTINCT last_name, COUNT(last_name)
+FROM employees
+WHERE (last_name LIKE '%q%')
+AND NOT (last_name LIKE '%qu%')
+GROUP BY last_name;
+
+SELECT COUNT(*)
+FROM employees
+WHERE (first_name = 'Irena' OR first_name = 'Vidya' OR first_name = 'Maya')
+GROUP BY gender;
