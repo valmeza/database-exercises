020274dd343f21396c5ed9f4e900de26ab88a469
Valeria Meza <valeriameza173@gmail.com>
2020-05-28 09:41:51 -0500

    feat minor change.

--- order_by_exercises.sql
+++ order_by_exercises.sql
@@ -4,7 +4,7 @@ SELECT *
 FROM employees
 WHERE gender = 'M'
   AND (first_name = 'Irena' OR first_name = 'Vidya' OR first_name = 'Maya')
-ORDER BY last_name, first_name ASC;
+ORDER BY last_name, first_name;
 
 SELECT *
 FROM employees
