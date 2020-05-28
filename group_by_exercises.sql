3ce3043bfec6390344f23c72a9e87c57fdb9250a
Valeria Meza <valeriameza173@gmail.com>
2020-05-28 13:54:47 -0500

    added aliases to the exercise.

--- group_by_exercises.sql
+++ group_by_exercises.sql
@@ -1,29 +1,29 @@
 USE employees;
 
-SELECT DISTINCT title
+SELECT DISTINCT title AS 'distinctTitles'
 FROM titles;
 
 SELECT DISTINCT last_name,
-                COUNT(last_name)
+                COUNT(last_name) AS 'lastNameCount'
 FROM employees
 WHERE last_name LIKE 'E%'
   AND last_name LIKE '%E'
 GROUP BY last_name;
 
-SELECT DISTINCT first_name, COUNT(first_name),
-                last_name, COUNT(last_name)
+SELECT DISTINCT first_name, COUNT(first_name) AS 'firstNameCount',
+                last_name, COUNT(last_name) AS 'lastNameCount'
 FROM employees
 WHERE (last_name LIKE 'E%')
 AND (last_name LIKE '%E')
 GROUP BY first_name, last_name;
 
-SELECT DISTINCT last_name, COUNT(last_name)
+SELECT DISTINCT last_name, COUNT(last_name) AS 'uniqueLastNameCount'
 FROM employees
 WHERE (last_name LIKE '%q%')
 AND NOT (last_name LIKE '%qu%')
 GROUP BY last_name;
 
-SELECT COUNT(*), gender
+SELECT COUNT(*) AS 'genderCount', gender
 FROM employees
 WHERE (first_name = 'Irena' OR first_name = 'Vidya' OR first_name = 'Maya')
 GROUP BY gender;
