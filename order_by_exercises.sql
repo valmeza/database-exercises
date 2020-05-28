906a3bb1fa12cc6b3eb65a3e22c84d842ca04865
Valeria Meza <valeriameza173@gmail.com>
2020-05-27 19:54:37 -0500

    completed order exercise.

--- order_by_exercises.sql
+++ order_by_exercises.sql
@@ -39,7 +39,7 @@ SELECT *
 FROM employees
 WHERE hire_date BETWEEN '1990-01-01' AND '1999-12-31'
   AND birth_date LIKE '%12-25%'
-ORDER BY birth_date, hire_date NOT LIKE '%1999%';
+ORDER BY birth_date, hire_date DESC;
 
 SELECT *
 FROM employees
